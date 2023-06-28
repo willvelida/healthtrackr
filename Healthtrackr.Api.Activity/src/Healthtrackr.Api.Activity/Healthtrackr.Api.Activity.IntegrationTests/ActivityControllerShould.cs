6a55de72bf6b8bce8ebfe99b2bcf3577b7202784
willvelida <willvelida@hotmail.co.uk>
2023-06-29 09:58:25 +1200

    Adding integration tests. Changing workflow

---
+++ Healthtrackr.Api.Activity/src/Healthtrackr.Api.Activity/Healthtrackr.Api.Activity.IntegrationTests/ActivityControllerShould.cs
@@ -0,0 +1,127 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Healthtrackr.Api.Activity.IntegrationTests
+{
+    public class ActivityControllerShould : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _applicationFactory;
+        private readonly HttpClient _httpClient;
+
+        public ActivityControllerShould(WebApplicationFactory<Program> applicationFactory)
+        {
+            _applicationFactory = applicationFactory;
+            _httpClient = _applicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnBadRequestWhenCallingGetActivityEnvelopeByDateWithInvalidDateFormat()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddDays(-5).ToString("dd-MM-yyyy");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activity?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.BadRequest);
+            response.Should().HaveError($"Date in format: {testDate} is invalid. Please provide a date in the format yyyy-MM-dd");
+        }
+
+        [Fact]
+        public async Task ReturnNotFoundWhenCallingGetActivityEnvelopeByDateWhenRecordIsNull()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddYears(1).ToString("yyyy-MM-dd");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activity?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.NotFound);
+            response.Should().HaveError($"No activity envelope found for {testDate}");
+        }
+
+        [Fact]
+        public async Task ReturnOkWhenCallingGetActivityEnvelopeByDateWithValidDate()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddDays(-5).ToString("yyyy-MM-dd");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activity?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task ReturnBadRequestWhenCallingGetActivitySummaryRecordByDateWithInvalidDate()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddDays(-5).ToString("dd-MM-yyyy");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activitysummary?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.BadRequest);
+            response.Should().HaveError($"Date in format: {testDate} is invalid. Please provide a date in the format yyyy-MM-dd");
+        }
+
+        [Fact]
+        public async Task ReturnNotFoundWhenCallingGetActivitySummaryRecordByDateWhenRecordIsNull()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddYears(1).ToString("yyyy-MM-dd");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activitysummary?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.NotFound);
+            response.Should().HaveError($"No activity summary found for {testDate}");
+        }
+
+        [Fact]
+        public async Task ReturnOkWhenCallingGetActivitySummaryRecordByDateWithValidDate()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddDays(-5).ToString("yyyy-MM-dd");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activitysummary?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task ReturnBadRequestWhenCallingGetActivitiesByDateWithInvalidDate()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddDays(-5).ToString("dd-MM-yyyy");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activities?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.BadRequest);
+            response.Should().HaveError($"Date in format: {testDate} is invalid. Please provide a date in the format yyyy-MM-dd");
+        }
+
+        [Fact]
+        public async Task ReturnOkWhenCallingGetActivitiesByDateByDateWithValidDate()
+        {
+            // ARRANGE
+            var testDate = DateTime.UtcNow.AddDays(-5).ToString("yyyy-MM-dd");
+
+            // ACT
+            var response = await _httpClient.GetAsync(Environment.GetEnvironmentVariable("BlUE_SLOT_URL") + $"/api/activities?date=${testDate}");
+
+            // ASSERT
+            response.Should().HaveStatusCode(HttpStatusCode.OK);
+        }
+    }
+}
--- Healthtrackr.Api.Activity/src/Healthtrackr.Api.Activity/Healthtrackr.Api.Activity/Program.cs
+++ Healthtrackr.Api.Activity/src/Healthtrackr.Api.Activity/Healthtrackr.Api.Activity/Program.cs
@@ -61,3 +61,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
